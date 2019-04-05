f675b027c3cde581524345018d41deabb88720b3
Sean Nealon <signalflowsean@gmail.com>
2019-04-05 17:02:30 -0400

    webcam is being rendered

--- dir/scripts/index.js
+++ dir/scripts/index.js
@@ -1,1 +1,6 @@
-console.log('hello world'); 
+const webcam = document.getElementById('webcam');
+
+if(navigator.mediaDevices.getUserMedia) { 
+  navigator.mediaDevices.getUserMedia({video : true})
+    .then(stream => webcam.srcObject = stream); 
+}
