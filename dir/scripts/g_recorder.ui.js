e0c5c008bbbe493ba84c98de6f9efec1f8a32e07
Sean Nealon <signalflowsean@gmail.com>
2019-04-09 15:27:47 -0400

    event handlers functionality added

--- dir/scripts/g_recorder.ui.js
+++ dir/scripts/g_recorder.ui.js
@@ -7,11 +7,18 @@ const gestureRecorderUI = () => {
   }; 
 
   const handleNewGestureClick = () => { 
-
+    $('#js-add-gesture').on('click', e => { 
+      e.preventDefault(); 
+      store.currState = 'naming'; 
+    }); 
   }; 
 
   const handleNewGestureSubmit = () => { 
-
+    $('#js-submit-gesture').on('submit', e => { 
+      e.preventDefault(); 
+      store.gestures.push(getGestureNameFromElement(e.currentTarget)); 
+      store.currState = 'counting-down'; 
+    }); 
   }; 
 
   const bindEventListeners = () => { 
