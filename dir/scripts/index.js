e9d147790f2eba68ae37ad9f730cc2b1ce5e40bb
Sean Nealon <signalflowsean@gmail.com>
2019-04-09 14:42:58 -0400

    jquery on ready

--- dir/scripts/index.js
+++ dir/scripts/index.js
@@ -1,9 +1,11 @@
+/* global $ */
+'use strict'
 
-$(() => { 
+$(document).ready(() => { 
   const webcam = document.getElementById('webcam');
 
   if(navigator.mediaDevices.getUserMedia) { 
     navigator.mediaDevices.getUserMedia({video : true})
       .then(stream => webcam.srcObject = stream); 
   }
-})
+}); 
