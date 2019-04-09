8352ec9672cdbd86d242652bb1692da8e7b64c1d
Sean Nealon <signalflowsean@gmail.com>
2019-04-09 15:15:01 -0400

    store has items

--- dir/scripts/store.js
+++ dir/scripts/store.js
@@ -1,9 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 'use strict'
 
+//all-states = ['ready', 'naming', 'counting-down', 'recording']
 const store = (() => { 
 
   return { 
-    
+    gestures: [], 
+    currState: 'ready',  
   }; 
 }); 
