5d6e91813c826a366e1d3fb41a0ead319da6c015
Sean Nealon <signalflowsean@gmail.com>
2019-04-12 14:03:42 -0400

    added boilderplate ajax request

--- src/client/scripts/file_saver.js
+++ src/client/scripts/file_saver.js
@@ -2,7 +2,20 @@
 
 const fileSaver = (() => { 
   const createFolder = (folderName) => { 
-    // makeDir(folderName); 
+    const folder = { folderName }
+    return fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, cors, *same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+          "Content-Type": "application/json",
+      },
+      redirect: "follow", // manual, *follow, error
+      referrer: "no-referrer", // no-referrer, *client
+      body: JSON.stringify(folder), // body data type must match "Content-Type" header
+  })
+  .then(response => response.json()); // parses JSON responce
   }; 
 
   const startRecording = () => { 
